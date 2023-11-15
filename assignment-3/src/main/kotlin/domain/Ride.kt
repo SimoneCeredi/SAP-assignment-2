7b1d998d814b5910c1b1e936ec7b90b126a174c3
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-11-15 10:00:30 +0100

    fix: changed user and escooter to identifiers in ride

--- assignment-3/src/main/kotlin/domain/Ride.kt
+++ assignment-3/src/main/kotlin/domain/Ride.kt
@@ -4,26 +4,26 @@ import java.util.*
 
 interface Ride {
     val id: String
-    val user: String
-    val escooter: String
+    val userId: String
+    val escooterId: String
     val startDate: Date
     var endDate: Date?
 }
 
 class RideImpl private constructor(
     override val id: String,
-    override val user: String,
-    override val escooter: String,
+    override val userId: String,
+    override val escooterId: String,
     override val startDate: Date,
     override var endDate: Date?,
 ) : Ride {
     companion object {
-        fun new(id: String, user: String, escooter: String) = RideImpl(id, user, escooter, Date(), null)
-        fun new(id: String, user: String, escooter: String, startDate: Date, endDate: Date?) =
-            RideImpl(id, user, escooter, startDate, endDate)
+        fun new(id: String, userId: String, escooterId: String) = RideImpl(id, userId, escooterId, Date(), null)
+        fun new(id: String, userId: String, escooterId: String, startDate: Date, endDate: Date?) =
+            RideImpl(id, userId, escooterId, startDate, endDate)
     }
 }
 
-fun Ride(id: String, user: String, escooter: String) = RideImpl.new(id, user, escooter)
-fun Ride(id: String, user: String, escooter: String, startDate: Date, endDate: Date?) =
-    RideImpl.new(id, user, escooter, startDate, endDate)
+fun Ride(id: String, userId: String, escooterId: String) = RideImpl.new(id, userId, escooterId)
+fun Ride(id: String, userId: String, escooterId: String, startDate: Date, endDate: Date?) =
+    RideImpl.new(id, userId, escooterId, startDate, endDate)
